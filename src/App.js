e2f4bcb7fc23c50b2b91b79581a1532eb09b9871
Happyasrat <120266685+Happyasrat@users.noreply.github.com>
2023-01-01 21:24:27 -0500

    App.js Updated,Files imported ,Apple replica made

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+
+import ".//commonResource/css/bootstrap.css";
+
+import ".//commonResource/css/styles.css"
+// import './App.css';
+import Header from './components/Header/Header';
+
+import Alerts from "./components/main/Alerts";
+import FirstSec from "./components/main/FirstSec";
+import SecondSec from "./components/main/SecondSec";
+
+import ThirdSec from "./components/main/ThirdSec";
+import FourthSec from "./components/main/FourthSec";
+import FifthSec from "./components/main/FifthSec";
+import SixthSec from "./components/main/SixthSec";
+import Footer from "./components/Footer/Footer";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <Alerts />
+
+      <FirstSec />
+      <SecondSec />
+      <ThirdSec />
+      <FourthSec />
+      <FifthSec />
+      <SixthSec/>
+      
+      
+      <Footer />
     </div>
   );
+ 
 }
 
 export default App;
